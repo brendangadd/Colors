0d40139cf9090bb7a2e4cafed89a2d541739ea78
Brendan Gadd <brendangadd@gmail.com>
2016-10-17 22:07:34 -0400

    Add basic colors support

---
+++ Colors.lua
@@ -0,0 +1,73 @@
+local TOKEN_PREFIX = '{{'
+local TOKEN_SUFFIX = '}}'
+
+local PATTERN_HEX_CODE = string.rep('[0-9a-fA-F]', 6)
+
+local WOW_COLOR = '\124cff'
+local WOW_RESET = '\124r'
+
+local defaultSendChatMessage = SendChatMessage;
+
+local colorsByName = {
+   red = 'ff4444',
+   green = '99cc00',
+   blue = '33b5e5',
+   purple = 'aa66cc',
+   orange = 'ffbb33'
+}
+
+function augmentedSendChatMessage(message, type, language, channel)
+   local lastResult = nil
+
+   while true do
+      local tStart, tEnd = string.find(message, TOKEN_PREFIX .. '.-' .. TOKEN_SUFFIX)
+      if not tStart then
+         break
+      end
+      local token = string.sub(message, tStart, tEnd)
+      local convertedToken = convertToken(token)
+      if string.len(convertedToken) > 0 then
+         lastResult = convertedToken
+      end
+      message = string.sub(message, 0, tStart - 1)
+         .. convertedToken
+         .. string.sub(message, tEnd + 1, string.len(message))
+   end
+
+   if lastResult and lastResult ~= WOW_RESET then
+      message = message .. WOW_RESET
+   end
+
+   defaultSendChatMessage(message, type, language, channel)
+end
+
+function convertToken(token)
+   local label = string.sub(
+      token,
+      string.len(TOKEN_PREFIX) + 1,
+      string.len(token) - string.len(TOKEN_PREFIX)
+   )
+
+   if string.sub(label, 1, 1) == '#' then
+      label = string.sub(label, 2, string.len(label))
+      if string.len(label) == 3 then
+         label = string.rep(string.sub(label, 1, 1), 2)
+            .. string.rep(string.sub(label, 2, 2), 2)
+            .. string.rep(string.sub(label, 3, 3), 2)
+      end
+      return string.find(label, PATTERN_HEX_CODE) and WOW_COLOR .. label or ''
+   end
+
+   if label == '/' then
+      return WOW_RESET
+   end
+
+   local hexCode = colorsByName[label]
+   if hexCode then
+      return WOW_COLOR .. hexCode
+   end
+
+   return ''
+end
+
+SendChatMessage = augmentedSendChatMessage
